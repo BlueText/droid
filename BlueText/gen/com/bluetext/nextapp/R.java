b6a639c9916ff7ebfbeed53b958854e00d3a98d2
aguibert <andy.guibert@gmail.com>
2013-12-08 23:11:40 -0600

    Polish gui for final version

--- BlueText/gen/com/bluetext/nextapp/R.java
+++ BlueText/gen/com/bluetext/nextapp/R.java
@@ -21,14 +21,15 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int bluetext_background1=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bluetext1=0x7f020000;
+        public static final int bluetext_background5=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int WelcomeText=0x7f080004;
+        public static final int WelcomeText=0x7f080003;
         public static final int action_settings=0x7f080005;
         public static final int button2=0x7f080002;
-        public static final int button_logout=0x7f080003;
+        public static final int button_logout=0x7f080004;
         public static final int passwordField=0x7f080000;
         public static final int phoneNumberField=0x7f080001;
     }
